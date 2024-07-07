fd971e9b824c5c74f374ef414ecc25b4e9a026fa
Wallis <wallis.sl@outlook.com>
2024-07-07 19:02:51 -0300

    FinalizacaoCadastroDeCarrinho

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express()
 
 const clientsRoutes = require('./src/routes/clients.routes');
 const productsRoutes = require('./src/routes/products.routes');
+const pedidosRoutes = require('./src/routes/pedidos.routes');
 
 
 app.use(express.json())
 app.use('/clients',clientsRoutes)
 app.use('/products',productsRoutes)
+app.use('/pedidos', pedidosRoutes)
 
 
 app.listen(3000, () => { console.log("servidor online")})
